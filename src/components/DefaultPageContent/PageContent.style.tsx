7304a69f7720f987ba12cabbde775d393d117b73
MarcosVinicius556 <marcosvinicios4132@gmail.com>
2024-05-11 01:59:13 -0300

    Página de Login estilizada

--- src/components/DefaultPageContent/PageContent.style.tsx
+++ src/components/DefaultPageContent/PageContent.style.tsx
@@ -4,6 +4,6 @@ export const DefaultPageContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    
+    width: 100%;
     height: 100%;
 `;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,7 +5,25 @@ const Login = () => {
   return (
     <DefaultPageContent>
       <LoginCard>
-        teste
+        <span>
+          <h1>Bem Vindo de Volta!</h1>
+          <small>Guarde e acesse todas as suas senhas em apenas um lugar, de forma rápida, prática e segura!</small>
+        </span>
+
+        <form>
+          <span>
+            <input type="text" placeholder='Nome de Usuário' autoFocus/>
+            <input type="password" placeholder='Senha'/>
+            <a href="#RecuperarSenha">Esqueceu sua senha?</a>
+
+            <input type="submit" value="Entrar" />
+          </span>
+        </form>
+
+        <h6>
+          <p>Ainda não possui uma conta? <a href='#Registre-se'>Registre-se</a></p>
+        </h6>
+
       </LoginCard>
     </DefaultPageContent>
   );
