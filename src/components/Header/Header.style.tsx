fe5a27c114f49838f0d55e3a097cf9272ee7fa9a
MarcosVinicius556 <marcosvinicios4132@gmail.com>
2024-05-11 00:46:27 -0300

    Header pronto

--- src/components/Header/Header.style.tsx
+++ src/components/Header/Header.style.tsx
@@ -1,7 +1,5 @@
 import { styled } from "styled-components";
 
 export const HeaderContainer = styled.header`
-    border: 2px solid red;
     width: 100%;
-    height: 70px;
 `;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,17 @@
+import { Container, Navbar, NavbarText } from "react-bootstrap";
 import { HeaderContainer } from "./Header.style";
+import { AiOutlineSafetyCertificate } from "react-icons/ai";
 
 export default function Header() {
     return(
-        <HeaderContainer>Cabeçalho</HeaderContainer>
+        <HeaderContainer>
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <AiOutlineSafetyCertificate size={35} color="green"/> {' '} PassGuard
+                    </Navbar.Brand>
+                </Container>
+            </Navbar>
+        </HeaderContainer>
     );
 }
